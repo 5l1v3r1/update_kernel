6204491b41312080780e6657eea7dc81efe172c6
Leon V <33411715+leonv024@users.noreply.github.com>
2018-03-14 21:32:08 +0100

    Changed progress bar handler

--- update_kernel_ui.sh
+++ update_kernel_ui.sh
@@ -18,48 +18,54 @@ esac
 mkdir ~/kernel
 
 if [ $? -eq 0 ]; then
-  wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.14.12/linux-headers-4.14.12-041412_4.14.12-041412.201801051649_all.deb -P ~/kernel/ -q  | tee >(${_zenity} --width=200 --height=100 \
-  				    --title="Downloading" --progress \
-			        --pulsate --text="Downloading new kernel...[1]" \
-              --auto-kill --auto-close \
-              --percentage=10) >${_out}
-  wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.14.12/linux-headers-4.14.12-041412-generic_4.14.12-041412.201801051649_amd64.deb -P ~/kernel/ -q  | tee >(${_zenity} --width=200 --height=100 \
-              --title="Downloading" --progress \
-              --pulsate --text="Downloading new kernel...[2]" \
-              --auto-kill --auto-close \
-              --percentage=10) >${_out}
+  (
+  echo "# Downloading new kernel...[1]"
+  wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.14.12/linux-headers-4.14.12-041412_4.14.12-041412.201801051649_all.deb -P ~/kernel/ -q
+  echo "20"
 
-  wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.14.12/linux-headers-4.14.12-041412-lowlatency_4.14.12-041412.201801051649_amd64.deb -P ~/kernel/ -q  | tee >(${_zenity} --width=200 --height=100 \
-              --title="Downloading" --progress \
-              --pulsate --text="Downloading new kernel...[3]" \
-              --auto-kill --auto-close \
-              --percentage=10) >${_out}
+  echo "# Downloading new kernel...[2]"
+  wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.14.12/linux-headers-4.14.12-041412-generic_4.14.12-041412.201801051649_amd64.deb -P ~/kernel/ -q
+  echo "30"
 
-  wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.14.12/linux-image-4.14.12-041412-generic_4.14.12-041412.201801051649_amd64.deb -P ~/kernel/ -q  | tee >(${_zenity} --width=200 --height=100 \
-              --title="Downloading" --progress \
-              --pulsate --text="Downloading new kernel...[4]" \
-              --auto-kill --auto-close \
-              --percentage=10) >${_out}
+  echo "# Downloading new kernel...[3]"
+  wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.14.12/linux-headers-4.14.12-041412-lowlatency_4.14.12-041412.201801051649_amd64.deb -P ~/kernel/ -q
+  echo "45"
 
-  wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.14.12/linux-image-4.14.12-041412-lowlatency_4.14.12-041412.201801051649_amd64.deb -P ~/kernel/ -q  | tee >(${_zenity} --width=200 --height=100 \
-              --title="Downloading" --progress \
-              --pulsate --text="Downloading new kernel...[5]" \
-              --auto-kill --auto-close \
-              --percentage=10) >${_out}
+  echo "# Downloading new kernel...[4]"
+  wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.14.12/linux-image-4.14.12-041412-generic_4.14.12-041412.201801051649_amd64.deb -P ~/kernel/ -q
+  echo "60"
+
+  echo "# Downloading new kernel...[5]"
+  wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.14.12/linux-image-4.14.12-041412-lowlatency_4.14.12-041412.201801051649_amd64.deb -P ~/kernel/ -q
+  echo "70"
+  echo "# Installing new kernel"
 
   dpkg -i ~/kernel/*.deb  | tee >(${_zenity} --width=200 --height=100 \
               --title="Downloading" --progress \
               --pulsate --text="Downloading new kernel...[6]" \
               --auto-kill --auto-close \
               --percentage=10) >${_out}
+  echo "100"
+  echo "# Finished updating kernel"
+
+  ) |
+  # Progress bar
+  ${_zenity} --progress --title "Status" --text "Downloading New kernel...[1]" --percentage=0 --auto-close --auto-kill
+
+  # Done
+  ${_zenity} --info --title "Information" --text "Done" --width "100" --height "50"
+
 else
+  # Error handler
   ${_zenity} --error --text="Error during installation... Canceled..."
+  rm -rf ~/kernel/
+  exit 1
 fi
 
 # Cleaning up
 rm -rf ~/kernel/
 
-zenity --question --title "Reboot Now?"
+zenity --question --title "Reboot" --text "Reboot Now?"
 case $? in
   0) echo "Rebooting..."; reboot
   ;;
