15bec4899bde5b4f79a16d13b660c40062529566
Leon V <33411715+leonv024@users.noreply.github.com>
2018-03-15 11:32:13 +0100

    Added version 4.15.9

--- update_kernel.sh
+++ update_kernel.sh
@@ -1,5 +1,4 @@
 #/bin/bash
-# Fix Meltdown Exploit (CVE-2017-5754)
 
 function pause() {
   read -sn 1 -p "Press any key to continue"
@@ -9,14 +8,14 @@ function pause() {
 function checkroot {
   if [ $USER == 'root' ]; then
     echo ''
-    echo -e "\e[1;93mInstalling Kernel update\e[0m \e[1;32mv4.14.12\e[0m"
-    pause; fix
+    echo -e "\e[1;93mInstalling Kernel update"
+    pause; select_kernel
   else
     echo -e '\e[1;31m[ERROR]\e[0m \e[1mRoot is required\e[0m'
   fi
 }
 
-function fix {
+function v41412 {
   echo 'Current Version: '; uname -r
   echo ''
   mkdir ~/kernel
@@ -39,7 +38,40 @@ function fix {
   echo -e '\e[1;32m[+]\e[0m\e[1;93m Installing New kernel\e[0m'
 
   echo -e '\e[1;96m'
-  dpkg -i ~/kernel/*.deb
+  #dpkg -i ~/kernel/*.deb
+  echo -e '\e[0m'
+
+  echo -e '\e[1;33m[ CLEANING UP ]\e[0m'
+  rm -rf ~/kernel/
+  echo -e '\e[1;32m[ DONE ]\e[0m'
+  #echo -e '\e[1;32mTo apply update please do:\e[0m \e[1;96mshutdown -r 0\e[0m'
+  reboot
+}
+
+function v4159 {
+  echo 'Current Version: '; uname -r
+  echo ''
+  mkdir ~/kernel
+  echo -e '\e[1;32m[+]\e[0m\e[1;93m ~/kernel/\e[0m'
+  echo -e '\e[1;32m[+]\e[0m\e[1;93m Downloading new kernel\e[0m'
+  wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.15.9/linux-headers-4.15.9-041509_4.15.9-041509.201803111231_all.deb -P ~/kernel/ -q
+
+  echo -e '\e[1;32m[+]\e[0m\e[1;93m Downloading new kernel\e[0m'
+  wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.15.9/linux-headers-4.15.9-041509-generic_4.15.9-041509.201803111231_amd64.deb -P ~/kernel/ -q
+
+  echo -e '\e[1;32m[+]\e[0m\e[1;93m Downloading new kernel\e[0m'
+  wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.15.9/linux-headers-4.15.9-041509-lowlatency_4.15.9-041509.201803111231_amd64.deb -P ~/kernel/ -q
+
+  echo -e '\e[1;32m[+]\e[0m\e[1;93m Downloading new kernel\e[0m'
+  wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.15.9/linux-image-4.15.9-041509-generic_4.15.9-041509.201803111231_amd64.deb -P ~/kernel/ -q
+
+  echo -e '\e[1;32m[+]\e[0m\e[1;93m Downloading new kernel\e[0m'
+  wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.15.9/linux-image-4.15.9-041509-lowlatency_4.15.9-041509.201803111231_amd64.deb -P ~/kernel/ -q
+
+  echo -e '\e[1;32m[+]\e[0m\e[1;93m Installing New kernel\e[0m'
+
+  echo -e '\e[1;96m'
+  #dpkg -i ~/kernel/*.deb
   echo -e '\e[0m'
 
   echo -e '\e[1;33m[ CLEANING UP ]\e[0m'
@@ -59,6 +91,22 @@ function reboot {
   else
     echo -e '\e[1;32m[ DONE ]\e[0m'
   fi
+}
+
+function select_kernel {
+  _menu_items=("v4.14.12" "v4.15.9" "Quit")
+  select menu in "${_menu_items[@]}"
+  do
+  	case $menu in
+  	"v4.14.12") clear; v41412; break
+  	;;
+  	"v4.15.9") clear; v4159; break
+  	;;
+  	"Quit") echo -e "Goodbye, \e[1;31m$USER\e[0m"; break 2
+  	;;
+  	*) echo -e "\e[1;35m[ ! ]\e[0m Not a valid selection!"; sleep 1s; clear; select_kernel; break
+  	esac
+  done
 }
 
 checkroot
